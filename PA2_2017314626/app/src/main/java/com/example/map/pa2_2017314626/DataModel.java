5ad15442ded5e79784607b8ab4a329f02988d064
jaehunLee-dev <jaehunlee.dev@gmail.com>
2022-06-22 23:08:42 +0900

    mobile_pa2

---
+++ PA2_2017314626/app/src/main/java/com/example/map/pa2_2017314626/DataModel.java
@@ -0,0 +1,17 @@
+package com.example.map.pa2_2017314626;
+
+public class DataModel {
+        public Items[] items;
+
+        public class Items{
+        public String thumbnail;
+        
+        public String getThumbnail() {
+            return thumbnail;
+        }
+
+        public void setThumbnail(String thumbnail) {
+            this.thumbnail = thumbnail;
+        }
+    }
+}
